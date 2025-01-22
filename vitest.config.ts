c4253c9c0643c51e6e8e74e608153c80aad16a54
Amin Yusuf <si.logoz@gmail.com>
2025-01-23 04:49:43 +0700

    configure the project

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setupTests.ts",
+    coverage: {
+      provider: "v8",
+    },
+  },
+});
