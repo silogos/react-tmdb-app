090fd7f431d663d8af62b3359860fdf10a59aa2a
Amin Yusuf <si.logoz@gmail.com>
2025-01-27 11:56:52 +0700

    fix: prevent background image update after component unmount

--- src/hooks/useBackgroundImage.ts
+++ src/hooks/useBackgroundImage.ts
@@ -4,15 +4,17 @@ import { useEffect } from "react";
 function useBackgroundImage(imagePath?: string) {
   useEffect(() => {
     if (!imagePath) return;
-
+    let isCancelled = false;
     const imageUrl = `https://image.tmdb.org/t/p/w300/${imagePath}`;
 
-    Promise.all([loadImage(imageUrl), wait(300)]).then(() => {
+    Promise.all([loadImage(imageUrl), wait(500)]).then(() => {
+      if (isCancelled) return;
       document.body.style.backgroundImage = `url('${imageUrl}')`;
       document.getElementById("backdrop-overlay")?.classList.add("opacity-80");
     });
 
     return () => {
+      isCancelled = true;
       document
         .getElementById("backdrop-overlay")
         ?.classList.remove("opacity-80");
